06063099302b44a242e3a68a837fc0593b67cb9a
petre <petr@enin.info>
2013-11-20 09:16:22 +0100

    pear update-channels and PHPUnit

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -23,4 +23,32 @@ class php
             ensure  => present,
             require => Exec['apt-get update']
     }
+
+    # upgrade pear
+    exec {
+        "pear upgrade":
+            command => "/usr/bin/pear upgrade",
+            require => Package['php-pear'],
+            returns => [ 0, '', ' ']
+    }
+
+    # set channels to auto discover
+    exec {
+        "pear auto_discover" :
+            command => "/usr/bin/pear config-set auto_discover 1",
+            require => [Package['php-pear']]
+    }
+
+    exec {
+        "pear update-channels" :
+            command => "/usr/bin/pear update-channels",
+            require => [Package['php-pear']]
+    }
+
+    exec {
+        "pear install phpunit":
+            command => "/usr/bin/pear install --alldeps pear.phpunit.de/PHPUnit",
+            creates => '/usr/bin/phpunit',
+            require => Exec['pear update-channels']
+    }
 }
